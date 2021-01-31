4fb3d6561d16d48d6b3f5b6091e8f713533b5c1a
ahall326 <38631780+ahall326@users.noreply.github.com>
2021-01-30 19:14:32 -0700

    first commit

--- lambda_function.py
+++ lambda_function.py
@@ -1,27 +1,31 @@
 import json
 import http.client
+import
 
 
 def lambda_handler(event, context):
-    conn = http.client.HTTPSConnection("hooks.slack.com")
 
-    payload = {"text": json.dumps(event)}
-
-    headers = {
-        'content-type': "application/json",
-        'cache-control': "no-cache",
-        'postman-token': "4c28ed21-a77f-5d5d-300b-6d33b3813bad"
-    }
-
-    conn.request(
-        "POST", "/services/TM49NLNKU/B01LLFBKHL4/OVUQhnpSx32jJg30m522f4Km", payload, headers)
-
-    res = conn.getresponse()
-    data = res.read()
-
-    print(data.decode("utf-8"))
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps(event)
-    }
+    matches = ["slow", "not loading", "page down", "can't access", "having issues"]
+    
+    if any(x in event['event']['text'] for x in matches):
+        conn = http.client.HTTPSConnection("hooks.slack.com")
+        payload = json.dumps({"text": json.dumps(event,indent=4, sort_keys=True)})
+        headers = {
+            'content-type': "application/json",
+            'cache-control': "no-cache",
+            'postman-token': "4c28ed21-a77f-5d5d-300b-6d33b3813bad"
+        }
+    
+        conn.request(
+            "POST", "/services/TM49NLNKU/B01LLFBKHL4/OVUQhnpSx32jJg30m522f4Km", payload, headers)
+    
+        res = conn.getresponse()
+        data = res.read()
+    
+        print(data.decode("utf-8"))
+    
+        return {
+            'statusCode': 200,
+            'body': json.dumps(event)
+        }
+    return none
